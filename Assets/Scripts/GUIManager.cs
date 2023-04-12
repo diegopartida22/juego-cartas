e435cf1297c0d2eaa2e3b16e3a1bec6c7ff6b6ca
diegopartida22 <A01641113@tec.mx>
2023-04-11 23:15:08 -0600

    Terminando el juego de las cartas

--- Assets/Scripts/GUIManager.cs
+++ Assets/Scripts/GUIManager.cs
@@ -2,22 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GUIManager : MonoBehaviour
 {
     [SerializeField]
     private CartaSO[] _cartas;
 
+    [SerializeField]
+    private TMP_Text _textoPokemonesDisponibles;
+
+        [SerializeField]
+    private TMP_Text _textoPokemonesDisponibles2;
+
     void Start()
     {
         DontDestroyOnLoad(gameObject);
         _cartas = Resources.LoadAll<CartaSO>("ScriptableObjects");
+        ActualizarTextoPokemonesDisponibles();
     }
 
     public void EscucharBoton()
     {
-        print("Boton presionado");
-
         List<CartaSO> cartasAleatorias = new List<CartaSO>();
         while (cartasAleatorias.Count < 5)
         {
@@ -33,6 +39,24 @@ public class GUIManager : MonoBehaviour
         {
             loaders[i].SetDatosCarta(cartasAleatorias[i]);
         }
+    }
+
+    public void VerPokemonesDisponibles(){
+        ActualizarTextoPokemonesDisponibles();
+    }
+
+    private void ActualizarTextoPokemonesDisponibles(){
+        string nombresPokemones = "";
+        for (int i = 0; i < 5; i++){
+            nombresPokemones += _cartas[i].nombre + "\n";
+        }
+        _textoPokemonesDisponibles.text = nombresPokemones;
+
+        string nombresPokemones2 = "";
+        for (int i = 5; i < 10; i++){
+            nombresPokemones2 += _cartas[i].nombre + "\n";
+        }
+        _textoPokemonesDisponibles2.text = nombresPokemones2;
     }
 
     public void Salir()
